2e2dc301b3ec4d5d0adce174216396ff31e645a3
blauswinburne <blau@swinburne.edu.my>
2018-09-05 17:36:32 +0800

    modified SnapGame.cs

--- src/SnapGame.cs
+++ src/SnapGame.cs
@@ -17,14 +17,30 @@ namespace CardGames
 		/// Respond to the user input -- with requests affecting myGame
 		/// </summary>
 		/// <param name="myGame">The game object to update in response to events.</param>
-		private static void HandleUserInput(Snap myGame)
+		private static void HandleUserInput (Snap myGame)
 		{
 			//Fetch the next batch of UI interaction
-			SwinGame.ProcessEvents();
+			SwinGame.ProcessEvents ();
 
-			if (SwinGame.KeyTyped (KeyCode.vk_SPACE))
+			if (SwinGame.KeyTyped (KeyCode.vk_SPACE)) 
 			{
 				myGame.FlipNextCard ();
+				//myGame.Start ();
+			}
+
+			if (myGame.IsStarted) 
+			{
+				if (SwinGame.KeyTyped (KeyCode.vk_LSHIFT) && SwinGame.KeyTyped (KeyCode.vk_RSHIFT)) 
+				{
+				} 
+				else if (SwinGame.KeyTyped (KeyCode.vk_LSHIFT)) 
+					{
+						myGame.PlayerHit (0);
+					} 
+				else if (SwinGame.KeyTyped (KeyCode.vk_RSHIFT)) 
+					{
+						myGame.PlayerHit (1);
+					}
 			}
 		}
 
@@ -34,7 +50,8 @@ namespace CardGames
 		/// <param name="myGame">The details of the game -- mostly top card and scores.</param>
 		private static void DrawGame(Snap myGame)
 		{
-			SwinGame.ClearScreen(Color.White);
+			SwinGame.DrawBitmap ("cardsBoard.png", 0, 0);
+			//SwinGame.ClearScreen(Color.White);
 
 			// Draw the top card
 			Card top = myGame.TopCard;
@@ -43,7 +60,7 @@ namespace CardGames
 				SwinGame.DrawText ("Top Card is " + top.ToString (), Color.RoyalBlue, 0, 20);
 				SwinGame.DrawText ("Player 1 score: " + myGame.Score(0), Color.RoyalBlue, 0, 30);
 				SwinGame.DrawText ("Player 2 score: " + myGame.Score(1), Color.RoyalBlue, 0, 40);
-				SwinGame.DrawCell (SwinGame.BitmapNamed ("Cards"), top.CardIndex, 350, 50);
+				SwinGame.DrawCell (SwinGame.BitmapNamed ("Cards"), top.CardIndex, 521, 153);
 			}
 			else
 			{
@@ -51,7 +68,7 @@ namespace CardGames
 			}
 
 			// Draw the back of the cards... to represent the deck
-			SwinGame.DrawCell (SwinGame.BitmapNamed ("Cards"), 52, 160, 50);
+			SwinGame.DrawCell (SwinGame.BitmapNamed ("Cards"), 52, 155, 153);
 
 			//Draw onto the screen
 			SwinGame.RefreshScreen(60);
